222faa1e6e3fa917a357113d7905a7647424a84d
Michael Sievers <michael_sievers@web.de>
2015-04-10 15:08:33 +0200

    Refactored tests

--- spec/mighty_tap_spec.rb
+++ spec/mighty_tap_spec.rb
@@ -1,44 +1,80 @@
 describe Object do
   describe "#mighty_tap" do
-    it "can be used like Object#tap" do
-      expect({}.mighty_tap { |_hash| _hash[:foo] = "bar" }[:foo]).to eq("bar")
-    end
+    let(:some_class) do
+      Class.new do
+        attr_accessor :state
 
-    context "if a method name is given" do
-      describe "it calls the method on the object" do
-        it "returns the object" do
-          expect([[1,2,3]].mighty_tap(:flatten!)).to eq([1,2,3])
+        def initialize(initial_state = "foo")
+          @state = initial_state
+        end
+
+        def state=(new_state = "bar", &block)
+          @state = new_state
+
+          if block_given?
+            yield @state
+          end
+        end
+
+        def upcase_state!
+          @state = @state.upcase
         end
       end
+    end
 
-      context "if parameters are given" do
-        describe "it calls the method on the object with the given parameters" do
-          it "returns the object" do
-            # example 1
-            expect([[[1,2,3]]].mighty_tap(:flatten!, 1)).to eq([[1,2,3]])
+    it "can be used like Object#tap" do
+      expect(
+        some_class.new.mighty_tap do |_some_object|
+          _some_object.state = "bar"
+        end.state
+      ).to eq("bar")
+    end
 
-            # example 2
-            double_integer = -> (integer) { integer * 2 }
-            expect([1,2,3].mighty_tap(:map!, double_integer)).to eq([1,2,3].map(&double_integer))
+    describe "it returns the object it was called on" do
+      context "if a method name is given" do
+        it "calls the method on the object" do
+          expect(some_class.new("foo").mtap(:upcase_state!).state).to eq("FOO")
+        end
+
+        context "if arguments are given" do
+          it "calls the method with the given arguments" do
+            expect(some_class.new("foo").mtap(:state=, "bar").state).to eq("bar")
+          end
+
+          context "if the last argument responds to :call" do
+            it "calls the method with the callable transformed into a block" do
+              expect(some_class.new("foo").mtap(:state=, -> (state) { state.clear << "bar" }).state).to eq("bar")
+            end
           end
         end
       end
-    end
 
-    context "if a block is given" do
-      describe "calls the block with itself" do
-        it "returns the object it was called on" do
-          expect([[[1,2,3]]].mtap { |_array| _array.flatten!(1) }).to eq([[1,2,3]])
+      context "if a callable is given" do
+        let(:state_changer) do
+          Class.new do
+            def call(object, new_state = "bar", &block)
+              object.state = new_state
+              yield object.state if block_given?
+            end
+          end.new
+        end
+
+        it "calls the callable with self" do
+          expect(some_class.new("foo").mtap(state_changer).state).to eq("bar")
+        end
+
+        context "if arguments are given" do
+          it "calls the callable with self and the given arguments" do
+            expect(some_class.new("foo").mtap(state_changer, "muff").state).to eq("muff")
+          end
+
+          context "the last argument responds to :call" do
+            it "calls the method with the callable transformed into a block" do
+              expect(some_class.new("foo").mtap(state_changer, "muff", -> (state) { state.upcase! }).state).to eq("MUFF")
+            end
+          end
         end
       end
-    end
-  end
-  
-  describe "#mtap" do
-    let(:object) { Object.new }
-
-    it "is an alias for #mighty_tap" do
-      expect(object.method(:mtap)).to eq(object.method(:mighty_tap))
     end
   end
 end
